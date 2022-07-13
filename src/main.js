61b4058f73129df4bab316a3b1ec0f58de4b8ede
InTheEnd <InTheEnd7777@outlook.com>
2022-07-13 12:07:08 +0800

    完成了初始化

--- src/main.js
+++ src/main.js
@@ -2,7 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import './styles/index.less'
+import './css/index.css'
+import Vant from 'vant'
+import 'vant/lib/index.css'
+import 'amfe-flexible/index'
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
